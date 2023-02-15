aa0e8c6dfe59205cc9bbc19e90bff3a778be7c3d
Yuki Mishima <41721281+s1534@users.noreply.github.com>
2023-02-15 23:16:57 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,82 @@
+from PyNuitrack import py_nuitrack
+import PyNuitrack
+import sys
+import cv2
+from itertools import cycle
+import numpy as np
+import time
+from collections import namedtuple
+import pyrealsense2 as rs
+import math
+import tifffile
+import csv
+from traceback import print_exc
+from pdb import set_trace
+import datetime
+from threading import Event, Thread
+import os
+import setproctitle
+from flask import Flask, render_template, request, make_response, jsonify,g
+import pandas as pd
+import pickle
+from collections import deque
+import json
+
+app = Flask(__name__)
+
+# process name
+setproctitle.setproctitle("m1_grandprix")
+# Setting CSV limit
+CSV_ROW_NUM = 150
+
+skeleton_list=[]
+skeleton_list_out=[]
+event = Event()
+
+eval_json = {
+    "evals" :[
+        0,20,40,60,80,100
+    ]
+}
+
+values = [0] * 50
+evals = deque(values)
+
+file = 'server_side/train_model/trained_model.pkl'
+random_forest_model = pickle.load(open(file,'rb'))
+
+
+def eval_skelton():
+    global eval_json
+    global evals
+    print('ここまでのものを評価します')
+    df = pd.read_csv(r"server_side/test_data/sample.csv")
+    test_x = df.drop(['action_label'],axis=1)
+    predict = random_forest_model.predict(test_x)
+    evals.append(predict[0])
+    evals.popleft()
+    eval_json["evals"] = list(evals)
+
+    with open('server_side/tmp.txt', 'w') as f:
+        for d in eval_json["evals"]:
+            f.write("%s\n" % d)
+
+@app.route('/')
+def index():
+    return 'Hello World'
+
+
+@app.route('/model')
+def model():
+    with open('eval.txt') as f:
+        lines = f.readlines()
+
+        lines = [line.rstrip('\n') for line in lines]
+    global eval_json
+    eval_json["evals"] = lines
+
+    return make_response(jsonify(eval_json))
+
+if __name__ == "__main__":
+
+    app.run(debug=True)
